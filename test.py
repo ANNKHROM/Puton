f7e23213e96fbfcf696f5b94a1062824e403696c
ANNKHROM <56717037+ANNKHROM@users.noreply.github.com>
2019-10-18 15:54:07 +0500

    Add files via upload

--- test.py
+++ test.py
@@ -1,5 +1,19 @@
-def inc(x):
-    return x + 1
-
-def test_answer():
-    assert inc(3) == 4
+import math
+ 
+print("Введите коэффициенты для квадратного уравнения (ax^2 + bx + c = 0):")
+a = float(input("a = "))
+b = float(input("b = "))
+c = float(input("c = "))
+ 
+discr = b ** 2 - 4 * a * c
+print("Дискриминант D = %.2f" % discr)
+ 
+if discr > 0:
+    x1 = (-b + math.sqrt(discr)) / (2 * a)
+    x2 = (-b - math.sqrt(discr)) / (2 * a)
+    print("x1 = %.2f \nx2 = %.2f" % (x1, x2))
+elif discr == 0:
+    x = -b / (2 * a)
+    print("x = %.2f" % x)
+else:
+    print("Корней нет")
